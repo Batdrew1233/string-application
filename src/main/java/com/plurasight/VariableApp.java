aa80002228212477a10776d67c0423231158a505
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-07 15:19:38 -0400

    Initial Commit

--- src/main/java/com/plurasight/VariableApp.java
+++ src/main/java/com/plurasight/VariableApp.java
@@ -1,4 +1,29 @@
 package com.plurasight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+//        Step 1
+        String favColor = "Blue";
+        int yearStarted = 2026;
+        char middleInital = 'J';
+        Boolean ifPets = false;
+        String niceMessage = "Have a wonderful day!";
+
+//        Step 2
+        int numDaysInWeek = 7;
+        System.out.println("There are " + numDaysInWeek + " days in a week.");
+
+        float coffeePrice = 4.99f;
+        System.out.println("Coffee cost $" + coffeePrice);
+
+        char favLetter = 'D';
+        System.out.println("My favorite letter is " + favLetter + ".");
+
+        Boolean isRaining = false;
+        System.out.println("Is it raining True or False: " + isRaining);
+
+
+
+
+    }
 }
